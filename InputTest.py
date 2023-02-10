636cd3ada235188d6c23740b900357b924113dbf
amba113 <64231693+amba113@users.noreply.github.com>
2023-02-10 09:24:56 -0500

    Worked on storing usernames, screen names, passwords...need to finish

--- InputTest.py
+++ InputTest.py
@@ -12,12 +12,16 @@ screen = pygame.display.set_mode(size)
 baseFont = pygame.font.Font(None, 40)
 userText1 = ''
 userText2 = ''
-inputRect1 = pygame.Rect(size[0]/2, size[1]/3, 150, 50)
-inputRect2 = pygame.Rect(size[0]/2, 2*size[1]/3, 150, 50)
+userText3 = ''
+inputRect1 = pygame.Rect(size[0]/2, size[1]/4, 150, 50)
+inputRect2 = pygame.Rect(size[0]/2, 2*size[1]/4, 150, 50)
+inputRect3 = pygame.Rect(size[0]/2, 3*size[1]/4, 150, 50)
 
 color = (255, 255, 255)
 box1 = False
 box2 = False
+box3 = False
+new = False
 
 while True:
     for event in pygame.event.get():
@@ -29,9 +33,15 @@ while True:
             if inputRect1.collidepoint(event.pos):
                 box1 = True
                 box2 = False
+                box3 = False
             elif inputRect2.collidepoint(event.pos):
                 box1 = False
                 box2 = True
+                box3 = False
+            elif inputRect3.collidepoint(event.pos):
+                box1 = False
+                box2 = False
+                box3 = True
   
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
@@ -39,20 +49,86 @@ while True:
                     userText1 = userText1[:-1]
                 elif box2:
                     userText2 = userText2[:-1]
+                elif box3:
+                    userText3 = userText3[:-1]
             elif event.key == pygame.K_RETURN:
-                print(userText1, userText2)
+                direct1 = "logins.txt"
+                f1 = open(direct1, 'r')
+                lines1 = f1.readlines()
+                direct2 = "usernames.txt"
+                f2 = open(direct2, 'r')
+                lines2 = f2.readlines()
+                user = ""
+                username = ""
+                newLines1 = []
+                good = False
+                for l in lines1:
+                    newLine1 = l.split("    ")
+                    newLines1 += [newLine1]
+                newLines2 = []
+                for l in lines2:
+                    newLine2 = l.split("    ")
+                    newLines2 += [newLine2]
+                                    
+                for i, line1 in enumerate(newLines1):
+                    for x, line2 in enumerate(newLines2):
+                        if userText1 == line1[0]:
+                            if userText2 + "\n" == line1[1]:
+                                good = True
+                            else:
+                                userText1 = ""
+                                userText2 = ""
+                                userText3 = ""
+                                good = False
+                                print("no match")
+                            new = False
+                        else:
+                            new = True
+                            
+                        if good == True:
+                            temp = newLines1[i]
+                            user = temp[0]
+                            username = temp[1]
+                f1.close()
+                f2.close()
+                print("User: " + user, "\nName: " + username)
+                            
             elif event.key == pygame.K_TAB:
                 if box1:
                     box1 = False
                     box2 = True
+                    box3 = False
                 elif box2:
+                    box1 = False
                     box2 = False
+                    box3 = True
+                elif box3:
                     box1 = True
+                    box2 = False
+                    box3 = False
             else:
                 if box1:
                     userText1 += event.unicode
                 elif box2:
                     userText2 += event.unicode
+                elif box3:
+                    userText3 += event.unicode
+        if new:
+            a1 = open(direct1, 'a')
+            a2 = open(direct2, 'a')
+            add1 = userText1 + "    " + userText2
+            add2 = userText1 + "    " + userText3
+            print(add1, "\n", add2)
+            a1.write(add1)
+            a2.write(add2)
+            print(a1, "\n", a2)
+            a1.close()
+            a2.close()
+            label3 = baseFont.render("Screen Name:", True, (0,0,0))
+            textSurface3 = baseFont.render(userText3, True, color)
+            pygame.draw.rect(screen, (0, 0, 0), inputRect3)
+            screen.blit(textSurface3, (inputRect3.x+5, inputRec3.y+5))
+            screen.blit(label3, (inputRect3.x, inputRect3.y - 30))
                 
     screen.fill(color)
     label1 = baseFont.render("Username:", True, (0,0,0))
@@ -66,6 +142,8 @@ while True:
     pygame.draw.rect(screen, (0, 0, 0), inputRect2)
     screen.blit(textSurface2, (inputRect2.x+5, inputRect2.y+5))
     screen.blit(label2, (inputRect2.x, inputRect2.y - 30))
+    
+    
     
     pygame.display.flip()
     clock.tick(60)
